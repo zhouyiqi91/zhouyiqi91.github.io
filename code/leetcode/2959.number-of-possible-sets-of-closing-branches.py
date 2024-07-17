94b2b80c9708b3cac386ccd21efce358b867f24a
zhouyiqi <386346852@qq.com>
2024-07-17 17:16:40 +0800

    update KMP

---
+++ code/leetcode/2959.number-of-possible-sets-of-closing-branches.py
@@ -0,0 +1,166 @@
+#
+# @lc app=leetcode.cn id=2959 lang=python3
+# @lcpr version=30204
+#
+# [2959] 关闭分部的可行集合数目
+#
+# https://leetcode.cn/problems/number-of-possible-sets-of-closing-branches/description/
+#
+# algorithms
+# Hard (59.61%)
+# Likes:    16
+# Dislikes: 0
+# Total Accepted:    5.5K
+# Total Submissions: 7.9K
+# Testcase Example:  '3\n5\n[[0,1,2],[1,2,10],[0,2,10]]'
+#
+# 一个公司在全国有 n 个分部，它们之间有的有道路连接。一开始，所有分部通过这些道路两两之间互相可以到达。
+# 
+# 公司意识到在分部之间旅行花费了太多时间，所以它们决定关闭一些分部（也可能不关闭任何分部），同时保证剩下的分部之间两两互相可以到达且最远距离不超过
+# maxDistance 。
+# 
+# 两个分部之间的 距离 是通过道路长度之和的 最小值 。
+# 
+# 给你整数 n ，maxDistance 和下标从 0 开始的二维整数数组 roads ，其中 roads[i] = [ui, vi, wi] 表示一条从
+# ui 到 vi 长度为 wi的 无向 道路。
+# 
+# 请你返回关闭分部的可行方案数目，满足每个方案里剩余分部之间的最远距离不超过 maxDistance。
+# 
+# 注意，关闭一个分部后，与之相连的所有道路不可通行。
+# 
+# 注意，两个分部之间可能会有多条道路。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 
+# 输入：n = 3, maxDistance = 5, roads = [[0,1,2],[1,2,10],[0,2,10]]
+# 输出：5
+# 解释：可行的关闭分部方案有：
+# - 关闭分部集合 [2] ，剩余分部为 [0,1] ，它们之间的距离为 2 。
+# - 关闭分部集合 [0,1] ，剩余分部为 [2] 。
+# - 关闭分部集合 [1,2] ，剩余分部为 [0] 。
+# - 关闭分部集合 [0,2] ，剩余分部为 [1] 。
+# - 关闭分部集合 [0,1,2] ，关闭后没有剩余分部。
+# 总共有 5 种可行的关闭方案。
+# 
+# 
+# 示例 2：
+# 
+# 
+# 
+# 输入：n = 3, maxDistance = 5, roads = [[0,1,20],[0,1,10],[1,2,2],[0,2,2]]
+# 输出：7
+# 解释：可行的关闭分部方案有：
+# - 关闭分部集合 [] ，剩余分部为 [0,1,2] ，它们之间的最远距离为 4 。
+# - 关闭分部集合 [0] ，剩余分部为 [1,2] ，它们之间的距离为 2 。
+# - 关闭分部集合 [1] ，剩余分部为 [0,2] ，它们之间的距离为 2 。
+# - 关闭分部集合 [0,1] ，剩余分部为 [2] 。
+# - 关闭分部集合 [1,2] ，剩余分部为 [0] 。
+# - 关闭分部集合 [0,2] ，剩余分部为 [1] 。
+# - 关闭分部集合 [0,1,2] ，关闭后没有剩余分部。
+# 总共有 7 种可行的关闭方案。
+# 
+# 
+# 示例 3：
+# 
+# 输入：n = 1, maxDistance = 10, roads = []
+# 输出：2
+# 解释：可行的关闭分部方案有：
+# - 关闭分部集合 [] ，剩余分部为 [0] 。
+# - 关闭分部集合 [0] ，关闭后没有剩余分部。
+# 总共有 2 种可行的关闭方案。
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 1 <= n <= 10
+# 1 <= maxDistance <= 10^5
+# 0 <= roads.length <= 1000
+# roads[i].length == 3
+# 0 <= ui, vi <= n - 1
+# ui != vi
+# 1 <= wi <= 1000
+# 一开始所有分部之间通过道路互相可以到达。
+# 
+# 
+#
+
+
+# @lcpr-template-start
+from string import *
+from re import *
+from datetime import *
+from collections import *
+from heapq import *
+from bisect import *
+from copy import *
+from math import *
+from random import *
+from statistics import *
+from itertools import *
+from functools import *
+from operator import *
+from io import *
+from sys import *
+from json import *
+from builtins import *
+from typing import *
+# @lcpr-template-end
+# @lc code=start
+class Solution:
+    def numberOfSets(self, n: int, maxDistance: int, roads: List[List[int]]) -> int:
+        ans = 0
+        for mask in range(1<<n):
+            op = set()
+            for i in range(n):
+                if not (1 << i) & mask:
+                    op.add(i)
+            g = [[inf]*n for _ in range(n)]
+            for x,y,d in roads:
+                if x in op and y in op:
+                    g[x][y] = d
+                    g[y][x] = d
+            for k in op:
+                for i in op:
+                    for j in op:
+                        if (d:=g[i][k] + g[k][j]) < g[i][j]:
+                            g[i][j] = d
+            
+            valid = True
+            for i in op:
+                for j in op:
+                    if i != j and g[i][j] > maxDistance:
+                        valid = False
+                        break
+            ans += valid
+
+        return ans
+
+
+
+
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# 3\n5\n[[0,1,2],[1,2,10],[0,2,10]]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# 3\n5\n[[0,1,20],[0,1,10],[1,2,2],[0,2,2]]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# 1\n10\n[]\n
+# @lcpr case=end
+
+#
+
--- notes/leetcode/字符串/KMP.md
+++ notes/leetcode/字符串/KMP.md
@@ -19,7 +19,64 @@ always j+1.
 
 > For each character c, imagine that we slide a copy of the pattern over these j characters (the first j-1 characters in the pattern followed by c—we are deciding what to do when these characters are txt.charAt(i-j+1..i)), from left to right, stopping when all overlapping characters match (or there are none). 
 
-
-
 > This gives the next possible place the pattern 
 could match. The index of the pattern character to compare with txt.charAt(i+1) (dfa[txt.charAt(i)][j]) is precisely the number of overlapping characters.
+
+## 算法导论 CLRS
+
+符号和术语
+T,P,偏移
+![](../../../images/20240717102323.png)
+
+前缀和后缀
+![](../../../images/20240717101946.png)
+
+找到所有偏移s, 使得P是T[1..s+m]的后缀
+![](../../../images/20240717102128.png)
+
+
+![](../../../images/20240717102657.png)
+
+## 笔记
+
+KMP 算法思想：在失配时，利用已经匹配的字符串，达到文本T的指针不会减少，只会减小模式P的指针，从而减少匹配次数。
+
+前缀表：next,fail,LPS(longest proper prefix which is also suffix)
+fail[i]: p[0..i]的最长相等前后缀的长度。在失配后，可以跳过最长相等的前缀，因为前缀和已经匹配的后缀相等。
+
+如何快速推导出fail数组：利用动态规划的思想，实际上也是KMP匹配的过程。
+
+# 模板
+
+```py
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+
+        def get_fail(p: str) -> List[int]:
+            n = len(p)
+            fail = [0] * n
+            j = 0
+            for i in range(1,n):
+                while j>0 and p[i] != p[j]:
+                    j = fail[j-1]
+                if p[i] == p[j]:
+                    j += 1
+                fail[i] = j
+            return fail
+        
+        n = len(haystack)
+        fail = get_fail(needle)
+        j = 0
+        for i in range(n):
+            while j > 0 and haystack[i] != needle[j]:
+                j = fail[j-1]
+            if haystack[i] == needle[j]:
+                j += 1
+            if j == len(needle):
+                return i-j+1
+        return -1
+```
+
+# 题目
+
+## [28\. 找出字符串中第一个匹配项的下标](https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string/)
